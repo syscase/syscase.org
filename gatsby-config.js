2b148b88bd9f8155443b409597a787dd4d978239
Firas Zaidan <firas@zaidan.de>
2020-01-07 17:30:17 +0100

    Add basic gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 'use strict';
 
 module.exports = {
+  siteMetadata: {
+    title: 'Syscase: A Fuzzing Framework for ARM TrustZone',
+    siteUrl: 'https://syscase.org'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ]
 }
