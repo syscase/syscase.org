54554941c860d288f97ca5f62036588374dfb6bf
Firas Zaidan <firas@zaidan.de>
2020-02-16 16:28:30 +0100

    Add layout to `pages/index.js`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => <div>Add content!</div>;
+import DefaultLayout from 'components/layouts/DefaultLayout';
+
+type Props = {
+  location: Location,
+};
+
+class Home extends Component<Props> {
+  render() {
+    const {location} = this.props;
+
+    return (
+      <DefaultLayout location={location}>
+        <div>Add more content!</div>
+      </DefaultLayout>
+    );
+  }
+}
+
+export default Home;
